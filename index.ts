f1bc7748abb8e68849d022b99f050de5028f62b9
Matt Schwartz <mattschwartz310@gmail.com>
2017-06-28 01:31:29 -0700

    added better docs

--- index.ts
+++ index.ts
@@ -5,16 +5,25 @@ import * as rmrf from 'rimraf';
 
 const { MongodHelper } = require('mongodb-prebuilt');
 
+const DEFAULT_PORT = 27017;
 const host: string = '127.0.0.1';
 const dbPath: string = path.join(__dirname, '__test-with-mongo-test-db__');
 
+/**
+ * Simple utility class to allow for easy unit, integration, and component testing
+ * with mongo. Manages spinning up instances and can delete dbs by name
+ */
 export class TestWithMongo {
     private port: number;
 
     constructor(port?: number) {
-        this.port = port || 27017;
+        this.port = port || DEFAULT_PORT;
     }
 
+    /**
+     * runs rm -rf on the directory containing the mongodb
+     * @returns Promise
+     */
     public clean(): Promise<any> {
         return new Promise((res: Function, rej: Function) => {
             rmrf(dbPath, (e: Error) => {
@@ -27,10 +36,19 @@ export class TestWithMongo {
         });
     }
 
+    /**
+     * gets the connection string for the test db with the name dbName
+     * @param dbName the name of the test db
+     * @returns connectionString
+     */
     public getConenctionString(dbName: string): string {
         return `mongodb://${host}:${this.port}/${dbName}`;
     }
 
+    /**
+     * starts mongod on the port set in the constructor
+     * @returns Promise
+     */
     public startMongoServer(): Promise<any> {
         const mongoHelper = new MongodHelper(['--port', this.port.toString(), '--dbpath', dbPath]);
 
@@ -39,6 +57,11 @@ export class TestWithMongo {
             .then(() => mongoHelper.run());
     }
 
+    /**
+     * Drops db with name dbName
+     * @param dbName nane of test db to drop
+     * @returns Promise
+     */
     public dropDb(dbName: string): Promise<any> {
         let db: Db;
         const connectionString = this.getConenctionString(dbName);
